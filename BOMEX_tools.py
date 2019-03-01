2151ce0cabbae0cdf2600b268105b144d9904509
mikejwx <mcjohnston2803@gmail.com>
2019-03-01 17:33:30 +0000

    Complete working figure 2

--- BOMEX_tools.py
+++ BOMEX_tools.py
@@ -7,54 +7,110 @@ from scipy import interpolate
 Functions to regrid winds and compute the resolved TKE for our simulations
 """
 
-def regrid_array(in_data, in_z, in_y, in_x, out_z, out_y, out_x):
+def regrid_array(in_data, in_z, in_y, in_x, out_z, out_y, out_x, verbose = True):
     """
     Takes a 3- or 4D array of input data and its coordinate axes, then regrids 
     that data to provided coordinate axes. Regridding is performed assuming no 
     change in resolution occurs.
     """
+    if verbose:
+        print 'Checking which dimensions need to be interpolated'
     # Test 1: Are they the same length?
     z_test = (len(in_z) == len(out_z)) # True = they're the same, False = they're different
     y_test = (len(in_y) == len(out_y))
     x_test = (len(in_x) == len(out_x))
     # Test 2: if they are the same length, do they have the same points?
     if z_test:
-        z_test = np.min([in_z[i] == out_z[i] for i in len(in_z)])
+        z_test = np.min([in_z[i] == out_z[i] for i in range(len(in_z))])
     if y_test:
-        y_test = np.min([in_y[i] == out_y[i] for i in len(in_y)])
+        y_test = np.min([in_y[i] == out_y[i] for i in range(len(in_y))])
     if x_test:
-        x_test = np.min([in_x[i] == out_x[i] for i in len(in_x)])
+        x_test = np.min([in_x[i] == out_x[i] for i in range(len(in_x))])
     
     if len(in_data.shape) == 3:
+        if verbose:
+            print '"in_data" is a 3D array'
         # Working with in_data[z, y, x]
         # Interpolate in the z-direction first
         if z_test:
+            if verbose:
+                print 'z-dimension does not need to be interpolated'
             out_data = in_data*1.
         else:
+            if verbose:
+                print 'Interpolating z-dimension'
             out_data = interpolate.interp1d(x = in_z, y = in_data, axis = 0, fill_value = 'extrapolate')(out_z)
         # Interpolate in the y-direction
-        if y_test:
-            out_data = interpolate.interp1d(x = in_y, y = out_data, axis = 1, fill_value = 'extrapolate')(out_y)
+        if not y_test:
+            if verbose:
+                print 'Interpolating y-dimension'
+            for i in xrange(in_y.shape[1]):
+                 out_data[:,:,i] = interpolate.interp1d(x = in_y[:,i], y = out_data[:,:,i], axis = 1, fill_value = 'extrapolate')(out_y[:,i])
+        else:
+            if verbose:
+                print 'y-dimension does not need to be interpolated'
         # Interpolate in the x-direction
-        if x_test:
-            out_data = interpolate.interp1d(x = in_x, y = out_data, axis = 2, fill_value = 'extrapolate')(out_x)
+        if not x_test:
+            if verbose:
+                print 'Interpolating x-dimension'
+            for j in xrange(in_x.shape[0]):
+                out_data[:,j,:] = interpolate.interp1d(x = in_x[j,:], y = out_data[:,j,:], axis = 1, fill_value = 'extrapolate')(out_x[j,:])
+        else:
+            if verbose:
+                print 'x-dimension does not need to be interpolated'
     elif len(in_data.shape) == 4:
+        if verbose:
+            print '"in_data" is a 4D array'
         # Working with in_data[t, z, y, x]
         # Interpolate in the z-direction first
         if z_test:
+            if verbose:
+                print 'z-dimension does not need to be interpolated'
             out_data = in_data*1.
         else:
+            if verbose:
+                print 'Interpolating the z-dimension'
             out_data = interpolate.interp1d(x = in_z, y = in_data, axis = 1, fill_value = 'extrapolate')(out_z)
         # Interpolate in the y-direction
-        if y_test:
-            out_data = interpolate.interp1d(x = in_y, y = out_data, axis = 2, fill_value = 'extrapolate')(out_y)
+        if not y_test:
+            if verbose:
+                print 'Interpolating the y-dimension'
+            for i in xrange(in_y.shape[1]):
+                out_data[:,:,:,i] = interpolate.interp1d(x = in_y[:,i], y = out_data[:,:,:,i], axis = 2, fill_value = 'extrapolate')(out_y[:,i])
+        else:
+            if verbose:
+                print 'y-dimension does not need to be interpolated'
         # Interpolate in the x-direction
-        if x_test:
-            out_data = interpolate.interp1d(x = in_x, y = out_data, axis = 3, fill_value = 'extrapolate')(out_x)
+        if not x_test:
+            if verbose:
+                print 'Interpolating the x-dimension'
+            for j in xrange(in_x.shape[0]):
+                out_data[:,:,j,:] = interpolate.interp1d(x = in_x[j,:], y = out_data[:,:,j,:], axis = 2, fill_value = 'extrapolate')(out_x[j,:])
+        else:
+            if verbose:
+                print 'x-dimension does not need to be interpolated'
     
     return out_data
 
-def get_tke(u, v, w, rho = 1.):
+
+
+
+    # 3. interpolate in y
+    if target_y_key != current_y_key:
+        print 'Regridding in latitude...'
+        # Interpolate in y
+        for i in xrange(current_data.shape[3]):
+            current_data[:,:,:,i] = interpolate.interp1d(current_y[:,i], current_data[:,:,:,i], axis = 2, fill_value = 'extrapolate')(target_y[:,i])
+        print 'Complete.'
+    # 4. interpolate in x
+    if target_x_key != current_x_key:
+        print 'Regridding in longitude...'
+        # Interpolate in x
+        for j in xrange(current_data.shape[2]):
+            current_data[:,:,j,:] = interpolate.interp1d(current_x[j,:], current_data[:,:,j,:], axis = 2, fill_value = 'extrapolate')(target_x[j,:])
+        print 'Complete.'
+
+def get_tke(u, v, w, rho = 1., verbose = True):
     """
     Function to compute the resolved TKE for u, v, and w wind components given 
     that they are all on the same grid.
@@ -78,12 +134,35 @@ def get_tke(u, v, w, rho = 1.):
     """
     nd = len(u.shape)
     if nd == 2:
+        if verbose:
+            print 'This is a 2D calculation'
         # 2D array
         TKE = 0.5*rho*((u - np.mean(u))**2. + (v - np.mean(v))**2. + (w - np.mean(w))**2.)
     elif nd == 3:
+        if verbose:
+            print 'This is a 3D calculation'
         # 3D array
-        TKE = 0.5*rho*((u - np.mean(u, axis = (1, 2)))**2. + (v - np.mean(v, axis = (1, 2)))**2. + (w - np.mean(w, axis = (1, 2)))**2.)
+        up = np.transpose(np.transpose(u) - np.mean(u, axis = (1, 2)))
+        vp = np.transpose(np.transpose(v) - np.mean(v, axis = (1, 2)))
+        wp = np.transpose(np.transpose(w) - np.mean(w, axis = (1, 2)))
+        TKE = 0.5*rho*(up**2. + vp**2. + wp**2.)
     elif nd == 4:
+        if verbose:
+            print 'This is a 4D calculation'
+            print 'Calculating the wind perturbations'
         # 4D array
-        TKE = 0.5*rho*((u - np.mean(u, axis = (2, 3)))**2. + (v - np.mean(v, axis = (2, 3)))**2. + (w - np.mean(w, axis = (2, 3)))**2.)
+        up = np.zeros_like(u)
+        vp = np.zeros_like(v)
+        wp = np.zeros_like(w)
+        u_mean = np.mean(u, axis = (2, 3))
+        v_mean = np.mean(v, axis = (2, 3))
+        w_mean = np.mean(w, axis = (2, 3))
+        for j in xrange(u.shape[2]):
+            for i in xrange(u.shape[3]):
+                if verbose:
+                    print '[j,i] = [' + str(j) + ',' + str(i) + ']'
+                up[:,:,j,i] = u[:,:,j,i] - u_mean
+                vp[:,:,j,i] = v[:,:,j,i] - v_mean
+                wp[:,:,j,i] = w[:,:,j,i] - w_mean
+        TKE = 0.5*rho*(up**2. + vp**2. + wp**2.)
     return TKE
--- figures.py
+++ figures.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
+from BOMEX_tools import *
+from scipy import integrate
 
 """
 Defining functions to plot the figures from Siebesma et al. (2003)
@@ -20,7 +22,7 @@ Figure 5: Profiles of (a) turbulent kinetic energy (TKE) and its vertical
           component (b) sigma_w^2.
 """
 
-def figure2(lwp_nc, lwp_key, lwp_time_key):
+def figure2(lwp_nc, lwp_key, lwp_time_key, viTKE, TKE_time, fig_name):
     """
     Compute the time series and plot them.
     """
@@ -45,7 +47,7 @@ def figure2(lwp_nc, lwp_key, lwp_time_key):
     ax1.text(10, 17.5, 'b)')
     # Panel (c)
     ax2 = fig.add_subplot(3, 1, 3)
-    #ax2.plot(tke_variables[time_key][:]*1., np.nanmean(viTKE, axis = (1, 2)), color = 'k', lw = 2)
+    ax2.plot(TKE_time, viTKE, color = 'k', lw = 2)
     ax2.set_xlim([0, 360])
     ax2.set_ylim([0, 750])
     ax2.set_xticks(np.arange(0, 361, 60))
@@ -54,18 +56,84 @@ def figure2(lwp_nc, lwp_key, lwp_time_key):
     ax2.set_ylabel('TKE (kgm$^{-1}$s$^{-2}$)')
     ax2.text(10, 625, 'c)')
     
+    plt.savefig('../Figure2_Siebesma2003_'+ fig_name + '.png', dpi = 150)
     plt.show()
-    #plt.savefig('../Figure2_Siebesma2003.png', dpi = 150)
 
-def main(path):
+def main(lwp_path, u_path, v_path, w_path, rho_path, fig_name, verbose = True):
     """
     Read in the netCDF data and process it to create variables for our plots
     """
+    if verbose:
+        print 'Defining keys for data variables'
     lwp_key = u'STASH_m01s30i405'
-    lwp_nc = Dataset(path + 'water.nc', 'r')
+    u_key   = u'STASH_m01s00i002'
+    v_key   = u'STASH_m01s00i003'
+    w_key   = u'STASH_m01s00i150'
+    rho_key = u'STASH_m01s00i389'
+    
+    if verbose:
+        print 'Opening netCDFs'
+    lwp_nc = Dataset(lwp_path, 'r')
+    u_nc = Dataset(u_path, 'r')
+    v_nc = Dataset(v_path, 'r')
+    w_nc = Dataset(w_path, 'r')
+    rho_nc = Dataset(rho_path, 'r')
+    
+    # Get the dimensions for my winds and rho
+    if verbose:
+        print 'Defining keys for the data dimensions'
+    z_u = 'rholev_zsea_rho'
+    z_v = 'rholev_zsea_rho'
+    z_w = 'thlev_zsea_theta'
+    z_rho = 'rholev_zsea_rho'
+    lat_u = 'latitude_cu'
+    lon_u = 'longitude_cu'
+    lat_v = 'latitude_cv'
+    lon_v = 'longitude_cv'
+    lat_w = 'latitude_t'
+    lon_w = 'longitude_t'
+    lat_rho = 'latitude_t'
+    lon_rho = 'longitude_t'
+    
+    w_time_shape = w_nc.variables[w_key][:].shape[0]
+    w_time_key = [key for key in w_nc.variables.keys() if ('min' in key) and (w_nc.variables[key].shape[0] == w_time_shape)][0]
+    
+    if verbose:
+        print 'Regridding the data for TKE calculation'
+        print 'regridding to "u_data"'
+    u_data = regrid_array(u_nc.variables[u_key][:]*1., u_nc.variables[z_u][:]*1., u_nc.variables[lat_u][:]*1., u_nc.variables[lon_u][:]*1., w_nc.variables[z_w][:], w_nc.variables[lat_w][:], w_nc.variables[lon_w][:]*1., verbose)
+    if verbose:
+        print 'regridding to "v_data"'
+    v_data = regrid_array(v_nc.variables[v_key][:]*1., v_nc.variables[z_v][:]*1., v_nc.variables[lat_v][:]*1., v_nc.variables[lon_v][:]*1., w_nc.variables[z_w][:], w_nc.variables[lat_w][:], w_nc.variables[lon_w][:]*1., verbose)
+    if verbose:
+        print 'regridding to "rho_data"'
+    rho_data = regrid_array(rho_nc.variables[rho_key][:]*1., rho_nc.variables[z_rho][:], rho_nc.variables[lat_rho][:]*1., rho_nc.variables[lon_rho][:]*1., w_nc.variables[z_w][:]*1., w_nc.variables[lat_w][:]*1., w_nc.variables[lon_w][:]*1., verbose)
+    
+    if verbose:
+        print 'Calculating the TKE'
+    TKE_time = w_nc.variables[w_time_key][:]
+    TKE = get_tke(u_data, v_data, w_nc.variables[w_key][:]*1., rho_data, verbose)
+    # Compute the vertical integral
+    if verbose:
+        print 'Integrating the TKE'
+    viTKE = np.mean(integrate.trapz(y = TKE, x = w_nc.variables[z_w][:]*1., axis = 1), axis = (1,2))
     lwp_time_shape = lwp_nc.variables[lwp_key][:].shape[0]
     lwp_time_key = [key for key in lwp_nc.variables.keys() if ('min' in key) and (lwp_nc.variables[key].shape[0] == lwp_time_shape)][0]
-    figure2(lwp_nc, lwp_key, lwp_time_key)
+    
+    if verbose:
+        print 'Plotting Figure 2'
+    figure2(lwp_nc, lwp_key, lwp_time_key, viTKE, TKE_time, fig_name)
+    lwp_nc.close()
+    u_nc.close()
+    v_nc.close()
+    w_nc.close()
+    rho_nc.close()
+    
+lwp_path = '/nerc/n02/n02/xb899100/BOMEX/Control/water.nc'
+u_path = '/nerc/n02/n02/xb899100/BOMEX/Control/wind.nc'
+v_path = '/nerc/n02/n02/xb899100/BOMEX/Control/wind.nc'
+w_path = '/nerc/n02/n02/xb899100/BOMEX/Control/wind.nc'
+rho_path = '/nerc/n02/n02/xb899100/BOMEX/Control/thermo.nc'
+my_fig_name = 'Control'
+main(lwp_path, u_path, v_path, w_path, rho_path, my_fig_name, verbose = False)
 
-path = '/nerc/n02/n02/xb899100/BOMEX/Control/'
-main(path)
